5c5d3f75ba3e647bc564d58e1ab4a89efad26650
adityasunny1189 <adityapathak1189@gmail.com>
2019-12-26 15:35:38 +0530

    patterns

--- pattern1.cpp
+++ pattern1.cpp
@@ -43,5 +43,31 @@ int main() {
     cout << endl;
   }
 
+//   Patten 3:-
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+cout << "Patten 4:-" << endl;
+for(int i = 0; i < 5 ; i++) {
+  for(int j = 0 ; j < 5 ; j++) {
+    if(i > j)
+      cout << " ";
+    else
+      cout << "*";
+  }
+  cout << endl;
+}
+
+// Patten 4:-
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
+
   return 0;
 }
